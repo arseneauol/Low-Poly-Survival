36d480ecf2ba32883a4f74b80b5bb65d955d4027
olivier arseneault <olivier arseneault>
2015-11-15 14:54:08 -0500

    nouvelle version du projet avec des option multi

---
+++ robot invation/Assets/script/GenericScripts/Framework/network/customTransformSync.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class customTransformSync : NetworkBehaviour {
+    [SerializeField]
+    float rotationInterpolation= 0.3f;
+    [SerializeField]
+    float potitionInterpolation= 0.08f;
+    Vector3 lastPosition;
+    float lastRotation;
+    Transform myTransform;
+    [SyncVar]
+    Vector3 sycPotition;
+    [SyncVar]
+    float sycRotationY;
+    void Start()
+    {
+        myTransform = GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        if (isLocalPlayer)
+        {
+            CmdSendMyPosition(myTransform.position);
+            CmdSendMyRotation(myTransform.eulerAngles.y);
+           
+        }
+        else
+        {
+            myTransform.position = Vector3.Lerp(myTransform.position, sycPotition, Time.deltaTime * 15);
+            myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.Euler(0, sycRotationY, 0), Time.deltaTime * 15);
+        }
+    }
+    [Command]
+    void CmdSendMyPosition(Vector3 potision)
+    {
+        sycPotition = potision;
+    }
+    [Command]
+    void CmdSendMyRotation(float RotaionY)
+    {
+        sycRotationY = RotaionY;
+    }
+
+}
---
+++ robot invation/Assets/script/multi_player_controleur.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class multi_player_controleur : NetworkBehaviour
+{
+    Transform myTransform;
+    Rigidbody myRigidbody;
+    [SerializeField]
+    float speed = 4;
+    GameObject myCamera;
+
+    // Use this for initializati
+    void Start()
+    {
+        myTransform = GetComponent<Transform>();
+        if (isLocalPlayer)
+        {
+            myCamera = myTransform.FindChild("camera").gameObject;
+            myCamera.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isLocalPlayer)
+        {
+            myTransform.Translate(new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * speed, 0, Input.GetAxis("Vertical") * Time.deltaTime * speed));
+        }
+    }
+}
